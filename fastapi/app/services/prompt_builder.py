bc7bc1e9cb134a11ba2a7a84c068fb76e4ed3a4a
CSchoice <tkdlqjf@gmail.com>
2025-05-10 21:55:52 +0900

    remove: 불필요한 파일 삭제

--- fastapi/app/services/prompt_builder.py
+++
@@ -1,5 +0,0 @@
-def build_finance_advice(scen):
-    if scen.label == "low_cash":
-        return (f"현재 유동성이 부족해 보입니다(L={scen.key_metrics['Liquidity']}). "
-                "다음 달 카드 사용을 30% 줄여보세요!")
-    return "재무 상태가 양호해 보이네요 👍"
